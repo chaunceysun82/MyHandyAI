c1ee7dfa97218206360ec87db3a08a8e411c7912
AlexEscobar11 <alex.escobar@laureate.net>
2025-07-28 15:22:27 -0400

    Update user.py

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -26,7 +26,7 @@ def create_user(user: User):
 
 @router.get("/login")
 def login_user(email: str, password: str):
-    user = users_collection.find_one({"_id": ObjectId(email)})
+    user = users_collection.find_one({"email": email})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
