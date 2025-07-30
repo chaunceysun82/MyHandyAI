e25aee85409d2e95a78f20c8ee624f5f5739ce9c
jyliang526 <ljy050526@gmail.com>
2025-07-30 14:01:03 -0500

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -2,7 +2,6 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.authentication import router as auth_router
 #from routes.profile_creation import router as profile_router
 from routes import user, project
 from mangum import Mangum
@@ -20,12 +19,10 @@ app.add_middleware(
 )
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello, FastAPI!"}
 
 # Register routes
-app.include_router(auth_router)
-# app.include_router(profile_router)
 app.include_router(user.router)
 app.include_router(project.router)
 
