9c749332587de0569144ff8e433f3af36f4931c1
tommy <tomyxc@outlook.com>
2025-08-04 23:00:14 -0400

    Add demo tool detection route using gpt Vision no history tracking until next setup

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,7 @@
 # Main.py
-
+from routes.tool_detection import router as tool_router
+from routes.chat import router as chat_router
+from routes.tool_detection_demo import router as demo_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.authentication import router as auth_router
@@ -9,7 +11,8 @@ from mangum import Mangum
 from fastapi import FastAPI
 
 app = FastAPI()
-
+app.include_router(chat_router)
+app.include_router(demo_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,6 +28,7 @@ async def root():
 # Register routes
 app.include_router(auth_router)
 # app.include_router(profile_router)
+app.include_router(tool_router)
 
 #handler for aws
 handler = Mangum(app)
