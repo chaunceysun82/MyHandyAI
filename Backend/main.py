4035f760312d3e0f460f7d08731fafaaabdd53b5
Rohit Lahori <rohitlahori.02@gmail.com>
2025-08-06 12:39:06 -0500

    Integrated endpoints for agentic chatbot

--- Backend/main.py
+++ Backend/main.py
@@ -1,15 +1,11 @@
 # Main.py
-from routes.chat import router as chat_router
-#from routes.tool_detection_demo import router as demo_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-#from routes.profile_creation import router as profile_router
-from routes import user, project, steps, chat
+from routes import user, project, steps, chat, chatbot
 from mangum import Mangum
 
-from fastapi import FastAPI
-
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -23,14 +19,11 @@ def root():
     return {"message": "Hello, FastAPI!"}
 
 # Register routes
-#app.include_router(auth_router)
-# app.include_router(profile_router)
-#app.include_router(demo_router)
 app.include_router(user.router)
 app.include_router(project.router)
 app.include_router(steps.router)
-app.include_router(chat_router)
-
+app.include_router(chat.router)
+app.include_router(chatbot.router)  # Add the new agentic chatbot router
 
 #handler for aws
 handler = Mangum(app)
