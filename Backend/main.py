96b45c9ef9795d3edc91569ba1ae20b3a97343d3
AlexEscobar11 <ale11davi@gmail.com>
2025-08-18 13:57:12 -0400

    trying to fix the deploy issue

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,7 @@
 # Main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import user, project, steps, chatbot, generation
+from routes import user, project, steps, generation
 from mangum import Mangum
 
 app = FastAPI()
@@ -23,7 +23,7 @@ app.include_router(user.router)
 app.include_router(project.router)
 app.include_router(steps.router)
 
-app.include_router(chatbot.router)
+#app.include_router(chatbot.router)
 app.include_router(generation.router)
 #app.include_router(langbot.router)
 
