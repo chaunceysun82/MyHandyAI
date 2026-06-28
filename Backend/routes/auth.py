7151b295df82158540a40e004a73bcb7d9d8f05d
Alex Ordonez <ale11davi@gmail.com>
2026-06-28 13:32:01 -0400

    debug /prod issue

--- Backend/routes/auth.py
+++ Backend/routes/auth.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 
 from security.cognito import get_current_cognito_user
 from security.current_user import sync_cognito_user
 
 router = APIRouter(prefix="/auth")
+
+
+@router.options("/me")
+def sync_current_user_preflight():
+    return Response(status_code=204)
 
 
 @router.post("/me")
